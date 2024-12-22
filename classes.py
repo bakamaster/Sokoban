28f0080f962a88a30156bdf04b4539a24ec37992
Bartosz Oliwier Pietrzak <01197515@pw.edu.pl>
2024-12-22 17:34:48 +0100

    Basic classes created

---
+++ classes.py
@@ -0,0 +1,58 @@
+class Tile:
+    def __init__(self, coordinateX, coordinateY):
+        self._coordinateX = coordinateX
+        self._coordinateY = coordinateY
+
+    def coordinateX(self):
+        return self._coordinateX
+
+    def coordinateY(self):
+        return self._coordinateY
+
+
+class NonMovableTile(Tile):
+    def __init__(self, coordinateX, coordinateY):
+        super().__init__(coordinateX, coordinateY)
+
+    def isMovable(self):
+        return False
+
+
+class MovableTile(Tile):
+    def __init__(self, coordinateX, coordinateY):
+        super().__init__(coordinateX, coordinateY)
+
+    def isMovable(self):
+        return True
+
+
+class Wall(NonMovableTile):
+    def __init__(self, coordinateX, coordinateY):
+        super().__init__(coordinateX, coordinateY)
+
+
+class EmptyTile(Tile):
+    def __init__(self, coordinateX, coordinateY):
+        super().__init__(coordinateX, coordinateY)
+
+
+class Switch(NonMovableTile):
+    def __init__(self, coordinateX, coordinateY):
+        super().__init__(coordinateX, coordinateY)
+        self._isActive = False
+
+    def isActive(self):
+        return self._isActive
+
+    def changeIsActive(self, state):
+        self._isActive = state
+
+
+class Box(MovableTile):
+    def __init__(self, coordinateX, coordinateY):
+        super().__init__(coordinateX, coordinateY)
+
+
+class Player(MovableTile):
+    def __init__(self, coordinateX, coordinateY):
+        super().__init__(coordinateX, coordinateY)
