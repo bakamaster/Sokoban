21c3f17051bb4017db35846107c1db6e25c88088
Bartosz Oliwier Pietrzak <01197515@pw.edu.pl>
2024-12-23 18:51:41 +0100

    added more move ooptions

--- gameManager.py
+++ gameManager.py
@@ -1,9 +1,23 @@
 from classes import EmptyTile
+from levelLoader import loadLevel
 board = {}
 
 
-def moveToRight(coordinateX, coordinateY):
-    originTile = board(coordinateX, coordinateY)
-    if str(board[(coordinateX + 1, coordinateY)]) == 'empty space':
-        board[(coordinateX + 1, coordinateY)] = originTile
+def move(coordinateX, coordinateY, direction):
+    originTile = board[(coordinateX, coordinateY)]
+    finaTile = (coordinateX + direction[0], coordinateY + direction[1])
+    if str(board[finaTile]) == 'empty space':
+        board[finaTile] = originTile
         board[(coordinateX, coordinateY)] = EmptyTile()
+    elif not str(board[finaTile]) == 'wall':
+        return False
+    elif str(board[finaTile]) == 'box':
+        if not move(finaTile[0], finaTile[1], direction):
+            pass
+        else:
+            move(finaTile[0], finaTile[1], direction)
+    elif str(board[finaTile]) == 'switch':
+        if board[finaTile].isActive():
+            pass
+        else:
+            board[finaTile] = originTile
--- levelLoader.py
+++ levelLoader.py
@@ -4,13 +4,16 @@ from classes import classSelector
 
 def loadLevel(path):
     board = {}
+    numberOfSwitches = 0
     x = 0
     y = 0
     with open(path, 'r') as file_handle:
         data = json.load(file_handle)
         for row in data:
             for tile in row:
+                if tile == 'switch':
+                    numberOfSwitches += 1
                 board[(x, y)] = classSelector(tile, x, y)
                 y += 1
             x += 1
-    return board
+    return board, numberOfSwitches
